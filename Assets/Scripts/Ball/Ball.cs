824b571f6f082cbdbf5810d358de99d87a1b5f3c
silvercrowkws <silvercrowkws@naver.com>
2026-04-28 16:22:12 +0900

    할 부분
    1. Monster_Goblin_Green 만 기능하는 인터페이스 다른 몬스터들에게도 이식
    2. 공의 데미지를 나중에 속성, 상성별로 기능하도록 수정 필요

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -110,7 +110,8 @@ public class Ball : RecycleObject
             // 충돌한 대상에게 damageable 인터페이스가 있으면
             if (damageable != null)
             {
-                damageable.TakeDamage(1f);
+                damageable.TakeDamage(10f);
+                // 나중에 속성 상성 이런 것 별로 기능하도록 수정 필요
             }
             
             // 반사 처리
