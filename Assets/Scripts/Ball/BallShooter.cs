7d968a5a158e32156f7fb4f769fe1b039ba9f4d0
silvercrowkws <silvercrowkws@naver.com>
2026-05-18 14:42:04 +0900

    젖음 상태, 화상 상태 처리
    화상일 때, 젖음이 된다면 남은 화상 대미지 전부 주고 젖음 상태로 변경.
    
    젖음 상태일 때, 화상이 된다면
    화상 상태로 넘어가지 않고 도트 대미지 전부 즉시 주고 젖음 상태 유지

--- Assets/Scripts/Ball/BallShooter.cs
+++ Assets/Scripts/Ball/BallShooter.cs
@@ -56,7 +56,8 @@ public class BallShooter : MonoBehaviour
         }
 
         // 첫 번째 공부터 속성 변경
-        shootBalls[0] = BallElementals.Fire;       
+        shootBalls[0] = BallElementals.Fire;
+        shootBalls[1] = BallElementals.Water;
 
 
         // 시작할 때는 가이드라인을 숨김
--- Assets/Scripts/Monster/MonsterBase.cs
+++ Assets/Scripts/Monster/MonsterBase.cs
@@ -174,13 +174,56 @@ public class MonsterBase : RecycleObject, IDamageable
         {
             // 화상 상태
             case StatusEffectType.Burn:
-                StartCoroutine(BurnCoroutine(effect));
+                //StartCoroutine(BurnCoroutine(effect));
+
+                // 젖음 상태라면 화상은 걸리지 않고 즉시 데미지만 적용
+                if (isWet)
+                {
+                    float instantDamage = effect.value * effect.duration;
+
+                    TakeDamage(instantDamage);
+
+                    Debug.Log($"젖음 상태에서 화상 반응! 즉시 데미지 : {instantDamage}");
+
+                    return;
+                }
+
+                Coroutine burn = StartCoroutine(BurnCoroutine(effect));
+                burnCoroutines.Add(burn);
                 break;
 
             // 젖음 상태
             case StatusEffectType.Wet:
-                StartCoroutine(WetCoroutine(effect));
-                //isWet = true;
+                //StartCoroutine(WetCoroutine(effect));
+
+                if (burnStackCount > 0)
+                {
+                    TakeDamage(remainBurnDamage);
+
+                    Debug.Log($"젖음 반응! 남은 화상 데미지 폭발 : {remainBurnDamage}");
+
+                    // 모든 화상 제거
+                    foreach (Coroutine coroutine in burnCoroutines)
+                    {
+                        if (coroutine != null)
+                        {
+                            StopCoroutine(coroutine);
+                        }
+                    }
+
+                    burnCoroutines.Clear();
+
+                    burnStackCount = 0;
+                    remainBurnDamage = 0f;
+                }
+
+                // 이미 젖음 상태라면 지속시간 갱신
+                if (wetCoroutine != null)
+                {
+                    StopCoroutine(wetCoroutine);
+                }
+
+                wetCoroutine = StartCoroutine(WetCoroutine(effect));
                 break;
 
             // 진흙 상태
@@ -202,25 +245,44 @@ public class MonsterBase : RecycleObject, IDamageable
     /// </summary>
     private int burnStackCount = 0;
 
+    /// <summary>
+    /// 현재 활성화된 화상 코루틴들
+    /// </summary>
+    private List<Coroutine> burnCoroutines = new List<Coroutine>();
+
+    /// <summary>
+    /// 남아있는 화상 총 누적 데미지
+    /// </summary>
+    private float remainBurnDamage = 0f;
+
+    /// <summary>
+    /// 화상 코루틴
+    /// </summary>
+    /// <param name="effect"></param>
+    /// <returns></returns>
     IEnumerator BurnCoroutine(StatusEffectData effect)
     {
         burnStackCount++;       // 화상 중첩 개수 증가
 
         float elapsedTime = 0f;
-
         float tickInterval = 1f;   // 1초마다 데미지
 
+        // 화상 총 누적 데미지 등록
+        remainBurnDamage += effect.value * effect.duration;
+
         // 첫 화상이면 이미지 활성화
-        if(burnStackCount == 1)
+        if (burnStackCount == 1)
         {
             StateEffectColorControl(true);
-            // 상태 이상 이미지 변경
-            statusEffectImage.sprite = burnSprite;
+            statusEffectImage.sprite = burnSprite;      // 상태 이상 이미지 변경
         }
 
         while (elapsedTime < effect.duration)
         {
             TakeDamage(effect.value);
+
+            // 실제 들어간 데미지만큼 감소
+            remainBurnDamage -= effect.value;
 
             yield return new WaitForSeconds(tickInterval);
 
@@ -229,17 +291,66 @@ public class MonsterBase : RecycleObject, IDamageable
 
         burnStackCount--;       // while 문 종료시 화상 중첩 개수 감소
 
-        // 화상 중첩이 하나도 안남았을 때만 끄기
+        // 현재 끝난 코루틴 정리
+        burnCoroutines.RemoveAll(c => c == null);
+
+        /*// 화상 중첩이 하나도 안남았을 때만 끄기
         if (burnStackCount <= 0)
         {
             burnStackCount = 0;
             StateEffectColorControl(false);
+        }*/
+
+        // 모든 화상 종료 시
+        if (burnStackCount <= 0)
+        {
+            burnStackCount = 0;
+            remainBurnDamage = 0f;
+
+            // 젖음 상태가 아니라면만 끄기
+            if (!isWet)
+            {
+                StateEffectColorControl(false);
+            }
         }
     }
 
+    /// <summary>
+    /// 현재 젖음 상태 코루틴
+    /// </summary>
+    private Coroutine wetCoroutine;
+
+    /// <summary>
+    /// 현재 젖음 상태 여부
+    /// </summary>
+    private bool isWet = false;
+
+    /// <summary>
+    /// 젖음 코루틴
+    /// </summary>
+    /// <param name="effect"></param>
+    /// <returns></returns>
     IEnumerator WetCoroutine(StatusEffectData effect)
     {
-        yield return null;
+        isWet = true;
+
+        // 상태 이상 이미지 표시
+        StateEffectColorControl(true);
+        statusEffectImage.sprite = wetSprite;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < effect.duration)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isWet = false;
+        wetCoroutine = null;
+
+        // 젖음 종료
+        StateEffectColorControl(false);
     }
 
     public virtual void OnDie()
